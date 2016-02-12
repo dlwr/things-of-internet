b34c5f62beeaf107e47f324e09e28c17722da0cb
yuta25 <kawai@pepabo.com>
2016-02-12 15:06:27 +0900

    initial

---
+++ app.js
@@ -0,0 +1,93 @@
+const Pageres = require('pageres')
+const fetch = require('node-fetch')
+const cheerio = require('cheerio')
+const cronJob = require('cron').CronJob
+const http = require('http')
+
+const options = {
+  selector: 'div.content',
+  css: 'div.right {display: none!important;}'
+}
+const API_KEY = "hogeraccho"
+
+const url = 'http://darekagakaku.herokuapp.com'
+
+var chunks = []
+var content = ''
+var prevContent = ''
+
+function darekagakaita() {
+  fetch(url)
+    .then(res => res.text())
+    .then(text => {
+      $ = cheerio.load(text)
+      content = $('div.content').text().replace(/About \| EditMe$/, '')
+      if (content === prevContent) {
+        console.log ('変わってなかった')
+        return Promise.reject()
+      }
+      return new Pageres(options)
+        .src('darekagakaku.herokuapp.com', ['1920x1080'])
+        .run()
+    })
+    .then(result => {
+      return new Promise((resolve, reject) => {
+        result[0].on('data', data => {
+          chunks.push(data)
+        })
+        result[0].on('end', () => {
+          var result = Buffer.concat(chunks)
+          resolve('data:image/png;base64,' + result.toString('base64'))
+        })
+      })
+    })
+    .then(img => {
+      var date = new Date
+      var itemId = [1,2,3,4,5,6,7,9][Math.floor(Math.random() * 8)]
+      var description = content.replace(/^\d{1,4}年\d{1,2}月\d{1,2}日/, '')
+      var params = {
+        texture: img,
+        title: date.getFullYear() + '年' + ('0' + (date.getMonth() + 1)).slice(-2) + '月' + date.getDate() + '日' + date.getHours() + '時' + date.getMinutes() + '分' + date.getSeconds() + '秒',
+        description: description,
+        products: [
+          {
+            itemId: itemId,
+            published: true,
+            resizeMode: 'contain'
+          }
+        ]
+      }
+      return fetch('https://suzuri.jp/api/v1/materials', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + API_KEY
+        },
+        method: 'POST',
+        body: JSON.stringify(params)
+      })
+    })
+    .then(res => {
+      console.log('変わってたから作った')
+      console.log(content)
+      prevContent = content
+    })
+}
+
+var job = new cronJob({
+  cronTime: '0,30 * * * * *',
+  onTick: darekagakaita,
+  start: true
+});
+
+job.start();
+
+http.createServer((request, response) => {
+  if (request.url === '/favicon.ico') {
+    response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    response.end();
+    console.log('favicon requested');
+    return;
+  }
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end(`誰かが書いた\n${prevContent}\n`)
+}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080)
